fd3597b41654d16c20953f12cd08e01c0d6fee32
23BFF0N\DXenu <talaikis.tadas@gmail.com>
2017-12-01 12:10:32 +0200

    update setup

--- tests.py
+++ tests.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
-import os
+from os import environ
+from os.path import join
 import sys
+from subprocess import Popen, TimeoutExpired
 
 import django
-from django.conf import settings
 from django.test.utils import get_runner
+from django.conf import settings
+
+
+def process_output(proc):
+    """
+    Communicates with process.
+    """
+    try:
+        outs, errs = proc.communicate(timeout=240)
+    except TimeoutExpired:
+        proc.kill()
+        outs, errs = proc.communicate()
+    return outs, errs
 
 
 if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = "backend.settings"
+    environ['DJANGO_SETTINGS_MODULE'] = "backend.settings"
     django.setup()
+
+    f = open(join(settings.BASE_DIR, "logs", "django.log"), "w+")
+    f.close()
+
+    #proc = Popen("chown -R www-data:www-data .")
+    #outs, errs = process_output(proc=proc)
+    #sys.exit(bool(errs))
+
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
-    
