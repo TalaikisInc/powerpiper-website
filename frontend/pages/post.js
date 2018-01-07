73f3e609ddc81b2c0188419cef5d8f99b8540585
23BFF0N\DXenu <talaikis.tadas@gmail.com>
2018-01-07 17:06:03 +0200

    adds social sharing

--- frontend/pages/post.js
+++ frontend/pages/post.js
@@ -9,6 +9,7 @@ import Heading from 'grommet/components/Heading'
 import Paragraph from 'grommet/components/Paragraph'
 import Image from 'grommet/components/Image'
 import Animate from 'grommet/components/Animate'
+import { FacebookButton, FacebookCount } from 'react-social'
 
 import Layout from './layout'
 import _Header from './components/Header'
@@ -24,26 +25,31 @@ export default class Post extends Component {
   }
 
   render () {
-    this.title = this.props.post.Title
-    this.description = this.props.post.Title
-    this.image = process.env.BASE_URL + '/' + this.props.post.Image
-    this.authorUrl = '/author/' + this.props.post.AuthorID.Username + '/0/'
-    this.categoryUrl = '/category/' + this.props.post.CategoryID.Slug + '/0/'
+    const title = this.props.post.Title
+    const description = this.props.post.Title
+    const image = process.env.BASE_URL + '/' + this.props.post.Image
+    const authorUrl = '/author/' + this.props.post.AuthorID.Username + '/0/'
+    const categoryUrl = '/category/' + this.props.post.CategoryID.Slug + '/0/'
+    const postUrl = process.env.BASE_URL + '/' + this.props.post.Slug + '/'
+    const fbAppId = process.env.FB_APP_ID
+    const twAppId = process.env.FB_APP_ID
+    const goAppId = process.env.FB_APP_ID
+    const liAppId = process.env.FB_APP_ID
 
     return (
       <App centered={false}>
         <Layout>
           <Article responsive={true} margin='none' flex={false} primary={true}>
-            {_Header({title: this.title, description: this. description, image: this.image})}
+            {_Header({ title: title, description: description, image: image })}
             <Section full={false} pad='medium' align='center' justify='center'>
               <Animate enter={{ animation: 'slide-up', duration: 1000, delay: 0 }} keep={true}>
                 <Heading align='center'>
-                  <a href={this.categoryUrl} className='grommetux-anchor' onMouseEnter={() => {Router.prefetch(this.categoryUrl)}}>
+                  <a href={categoryUrl} className='grommetux-anchor' onMouseEnter={() => {Router.prefetch(this.categoryUrl)}}>
                     {this.props.post.CategoryID.Title}
                   </a>  >> {this.props.post.Title} 
                 </Heading>
                 <p>
-                  By <a href={this.authorUrl} className='grommetux-anchor' onMouseEnter={() => {Router.prefetch(this.authorUrl)}}>
+                  By <a href={this.authorUrl} className='grommetux-anchor' onMouseEnter={() => { Router.prefetch(authorUrl) }}>
                     {this.props.post.AuthorID.FirstName} {this.props.post.AuthorID.LastName}
                   </a>
                   &nbsp;|&nbsp;
@@ -53,6 +59,24 @@ export default class Post extends Component {
                 <Paragraph align='start' size='large'>
                   <div dangerouslySetInnerHTML={{ __html: this.props.post.Content}} />
                 </Paragraph>
+                <div>
+                  <FacebookButton url={postUrl} appId={fbAppId}>
+                    <FacebookCount url={postUrl} />
+                    {' Share ' + postUrl }
+                  </FacebookButton>
+                  <TwitterButton url={postUrl} appId={twAppId}>
+                    <TwitterCount url={postUrl} />
+                    {' Share ' + postUrl }
+                  </TwitterButton>
+                  <GooglePlusButton url={postUrl} appId={goAppId}>
+                    <GooglePlusCount url={postUrl} />
+                    {' Share ' + postUrl }
+                  </GooglePlusButton>
+                  <LinkedInButton url={postUrl} appId={liAppId}>
+                    <LinkedInCount url={postUrl} />
+                    {' Share ' + postUrl }
+                  </LinkedInButton>
+                </div>
               </Animate>
             </Section>
           </Article>
